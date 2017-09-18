fc9acc72c7a0c1af969f9130b0a05521f36dcc0c
Reuben Balik <rsbalik@gmail.com>
2017-09-18 00:10:53 -0500

    Add fact generation endpoint

--- expenditures/app.py
+++ expenditures/app.py
@@ -3,13 +3,23 @@
 from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
 from redis import Redis
+import firebase_admin
+from firebase_admin import credentials, db
 
+
+import collections
 import datetime as dt
 import json
+import random
 import requests
+import os
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
+cred = credentials.Certificate('/code/firebaseServiceAccountKey.json')
+firebase_app = firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://illinois-calc.firebaseio.com/'
+    })
 
 candidates = [
     {
@@ -74,6 +84,15 @@ candidates = [
     },
 ]
 
+day_to_other_time = collections.OrderedDict(
+        [('second', (1/(24*60*60))),
+        ('minute', (1/(24*60))),
+        ('hour', (1/24)),
+        ('day', 1),
+        ('week', 7),
+        ('month', 30)]
+        )
+
 # how long our cached items last
 redisDuration = 3600 # one hour
 # assume there will never be more than 1,000,000 expenditures (but you never know, amirite?)
@@ -97,6 +116,14 @@ def calculateSpentPerSecond(perDay):
     return perDay / 86400
 
 
+def getBestTimespanForSpend(spentPerDay, fact_amount):
+    for timespan, ratio in day_to_other_time.iteritems():
+        spentPerTime = spentPerDay * ratio
+        if spentPerTime >= fact_amount:
+            return timespan
+    return None
+
+
 # convenience during development/testing
 @app.route('/clear', methods=['GET'])
 def clear():
@@ -105,10 +132,50 @@ def clear():
 
     return 'cache cleared'
 
-
-@app.route('/candidate/<string:candidate_nick>', methods=['GET'])
+@app.route('/expenditures/facts/random', methods=['GET'])
 @cross_origin()
-def get_candidate(candidate_nick):
+def get_random_fact():
+    global app, candidates, day_to_other_time
+    # pick a random fact from the db
+    # pick a random candidate and get their numbers
+    # calculate stuff and return the text
+    facts_ref = db.reference('facts')
+    lastfact = facts_ref.order_by_key().limit_to_last(1).get()
+    for key in lastfact:
+        max_fact_id = key
+    rand_fact_id = random.randrange(1, int(max_fact_id))
+
+    fact_ref = db.reference('facts/%d'%rand_fact_id)
+    rand_fact = fact_ref.get()
+
+    rand_cand = random.choice(candidates)
+    cand_expenditures = get_cand_expenditures(rand_cand['id'])
+    app.logger.info(str(cand_expenditures))
+
+    # get it before rounding
+    spentPerDay = calculateSpentPerDay(float(cand_expenditures['spendingDays']),
+            float(cand_expenditures['total']))
+
+    timespan = getBestTimespanForSpend(spentPerDay, float(rand_fact['amount']))
+    if timespan is None:
+        # seems like this shouldn't happen if we're picking the right facts, but may need to find
+        # a better thing to do here
+        text = 'We couldn\'t find a good timespan for this fact and candidate'
+    else:
+        text = '%s spends more in one %s than %s. Source: %s'%(
+                rand_cand['name'],
+                timespan,
+                rand_fact['item'],
+                rand_fact['source'])
+    resp = {'text':text}
+    return jsonify(resp)
+
+
+
+
+
+
+def get_cand_expenditures(candidate_nick):
     # find a matching committee_id
     committeeId = None
 
@@ -155,9 +222,15 @@ def get_candidate(candidate_nick):
 
             # store API call results in redis for one hour
             redis.setex(candidate_nick, json.dumps(responseJSON), redisDuration)
+    return responseJSON
 
+
+
+@app.route('/candidate/<string:candidate_nick>', methods=['GET'])
+@cross_origin()
+def get_candidate(candidate_nick):
     # return JSON data about requested candidate *or* error message
-    return jsonify(responseJSON)
+    return jsonify(get_cand_expenditures(candidate_nick))
 
 
 # might as well have something on the home page, eh?
@@ -165,8 +238,8 @@ def get_candidate(candidate_nick):
 def index():
     hits = redis.get('indexhits')
 
-    if (hits and int(hits) > 100):
-        strang = 'You know why you visited this time, but what do you think the other {} visits were about?'.format(hits - 1)
+    if (hits and int(hits) > 2):
+        strang = 'You know why you visited this time, but what do you think the other {} visits were about?'.format(int(hits) - 1)
     else:
         strang = 'My, how nice of you to visit.'
 
